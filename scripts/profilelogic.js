a7d63a139b670629df60326dc6aa501bafa54e83
sryoyin <youyingaming@gmail.com>
2026-04-24 08:11:14 -0500

    update 7

--- scripts/profilelogic.js
+++ scripts/profilelogic.js
@@ -14,22 +14,31 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 onAuthStateChanged(auth, (user) => {
+    const accdata = [document.getElementById('accimg'), document.getElementById('accname'), document.getElementById('accdate'), document.getElementById('accID')];
     if (user) {
     // Nombre y Foto
-    document.getElementById('accimg').src = user.photoURL || "img/student.png";
-    document.getElementById('accname').textContent = user.displayName || user.email.split('@')[0];
+    accdata[0].src = user.photoURL || "img/student.png";
+    accdata[1].textContent = user.displayName || user.email.split('@')[0];
     
     // Fecha de creación
     const fecha = new Date(user.metadata.creationTime).toLocaleDateString();
-    document.getElementById('accdate').textContent = `Member since: ${fecha}`;
+    accdata[2].textContent = `Member since: ${fecha}`;
     
     // ID
-    document.getElementById('accID').textContent = `Student ID: ${user.uid.substring(0, 8).toUpperCase()}`;
+    accdata[3].textContent = `Student ID: ${user.uid.substring(0, 8).toUpperCase()}`;
     }
+
+    // Opacidad
+    accdata.forEach(value => {
+        if (value) {
+            value.style.opacity = "1";
+        }
+    });
 });
 
 // --- LOGOUT ---
 const logoutBtn = document.getElementById('logout-btn');
+
 if (logoutBtn) {
     logoutBtn.addEventListener('click', async () => {
         try {
@@ -38,5 +47,5 @@ if (logoutBtn) {
         } catch (error) {
             console.error("Error al salir:", error);
         }
-    })
-};
+    });
+}
